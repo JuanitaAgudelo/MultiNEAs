7924d19a46b6d15f164a523e81d4428aebb1b174
Web Developer <jorge.zuluaga@udea.edu.co>
2026-01-22 03:51:34 -0500

    chore: sync tracked changes

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
-    version='0.2.14',
+    version='0.2.15',
 
     # ######################################################################
     # FILES
